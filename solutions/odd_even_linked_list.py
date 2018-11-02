ac4a33cc8df791ca1257479c37a7f0a4433db846
Daniel Ong <danielong1@gmail.com>
2018-11-01 23:35:50 -0700

    add new solution and renamed files

---
+++ solutions/odd_even_linked_list.py
@@ -0,0 +1,25 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        even_head = head.next
+        while even and even.next:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
+        odd.next = even_head
+        return head
+

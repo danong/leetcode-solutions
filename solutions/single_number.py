51e9227601ef855c894384e6415bdb1ba414f575
Daniel Ong <danielong1@gmail.com>
2018-11-05 23:13:26 -0800

    Create single_number.py

---
+++ solutions/single_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        xor = 0
+        for num in nums:
+            xor = xor ^ num
+        return xor
+        

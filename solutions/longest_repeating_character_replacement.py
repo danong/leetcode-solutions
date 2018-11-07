9d332d921ac06c0ef0686412b20933372894175b
Daniel Ong <danielong1@gmail.com>
2018-11-06 22:28:07 -0800

    Add sliding window solution

--- solutions/longest_repeating_character_replacement.py
+++ solutions/longest_repeating_character_replacement.py
@@ -1,4 +1,5 @@
 """https://leetcode.com/problems/longest-repeating-character-replacement/description/"""
+from collections import defaultdict
 
 
 class Solution:
@@ -8,3 +9,29 @@ class Solution:
         :type k: int
         :rtype: int
         """
+        if not s:
+            return 0
+
+        char_counts = defaultdict(int)
+
+        start = end = length = 0
+
+        while end < len(s):
+            end_char = s[end]
+            char_counts[end_char] += 1
+            end += 1
+            counter = end - start - max(char_counts.values())
+
+            while counter > k:
+                start_char = s[start]
+                char_counts[start_char] -= 1
+                start += 1
+                counter = end - start - max(char_counts.values())
+
+            length = max(length, end - start)
+        return length
+
+
+a = Solution()
+print(a.characterReplacement('ABAB', 2))
+print(a.characterReplacement('AABABBA', 1))

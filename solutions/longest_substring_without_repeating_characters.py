d72b921aec7792ba0eac874e2c096e919b5e948b
Daniel Ong <danielong1@gmail.com>
2018-11-06 21:38:48 -0800

    Add new sliding window solution

--- solutions/longest_substring_without_repeating_characters.py
+++ solutions/longest_substring_without_repeating_characters.py
@@ -17,3 +17,24 @@ class Solution:
             max_seen = max(max_seen, end_idx - start_idx + 1)
         
         return max_seen
+
+    def lengthOfLongestSubstring2(self, s):
+        if not s:
+            return 0
+
+        seen = {}
+        start_idx = 0
+        end_idx = 0
+        max_seen = 0
+
+        while end_idx < len(s):
+            cur = s[end_idx]
+            if cur in seen and seen[cur] >= start_idx:
+                start_idx = seen[cur] + 1
+            else:
+                seen[cur] = end_idx
+                end_idx += 1
+
+            if (end_idx - start_idx) > max_seen:
+                max_seen = end_idx - start_idx
+        return max_seen

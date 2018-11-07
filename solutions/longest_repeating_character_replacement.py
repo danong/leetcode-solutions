9bbc46577eae1c6a1abc367fb5de4c3751e91b39
Daniel Ong <danielong1@gmail.com>
2018-11-06 22:12:15 -0800

    Create longest_repeating_character_replacement.py

---
+++ solutions/longest_repeating_character_replacement.py
@@ -0,0 +1,10 @@
+"""https://leetcode.com/problems/longest-repeating-character-replacement/description/"""
+
+
+class Solution:
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """

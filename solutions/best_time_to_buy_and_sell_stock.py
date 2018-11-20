6532d4de8cfa44521b474c5f4f1f9432d60ac8f8
Daniel Ong <danielong1@gmail.com>
2018-11-20 09:46:59 -0800

    Create best_time_to_buy_and_sell_stock.py

---
+++ solutions/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+"""https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"""
+
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
